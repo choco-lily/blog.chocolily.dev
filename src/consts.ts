3331e6f810e3244dbff669faacb62d3a67c6f05c
chocolily <medeia_beliar@naver.com>
2025-03-28 21:16:25 +0900

    prettier, change asset url to cdn

--- src/consts.ts
+++ src/consts.ts
@@ -1,37 +1,36 @@
 export type Site = {
-  TITLE: string
-  DESCRIPTION: string
-  EMAIL: string
-  NUM_POSTS_ON_HOMEPAGE: number
-  POSTS_PER_PAGE: number
-  SITEURL: string
+    TITLE: string
+    DESCRIPTION: string
+    EMAIL: string
+    NUM_POSTS_ON_HOMEPAGE: number
+    POSTS_PER_PAGE: number
+    SITEURL: string
 }
 
 export type Link = {
-  href: string
-  label: string
+    href: string
+    label: string
 }
 
 export const SITE: Site = {
-  TITLE: 'chocolily',
-  DESCRIPTION:
-    'chocolily\'s blog',
-  EMAIL: 'x@chocolily.dev',
-  NUM_POSTS_ON_HOMEPAGE: 4,
-  POSTS_PER_PAGE: 4,
-  SITEURL: 'https://blog.chocolily.dev',
+    TITLE: "chocolily",
+    DESCRIPTION: "chocolily's blog",
+    EMAIL: "x@chocolily.dev",
+    NUM_POSTS_ON_HOMEPAGE: 4,
+    POSTS_PER_PAGE: 4,
+    SITEURL: "https://blog.chocolily.dev",
 }
 
 export const NAV_LINKS: Link[] = [
-  { href: '/blog', label: 'blog' },
-  { href: '/authors', label: 'authors' },
-  { href: '/about', label: 'about' },
-  { href: '/tags', label: 'tags' },
+    { href: "/blog", label: "blog" },
+    { href: "/authors", label: "authors" },
+    { href: "/about", label: "about" },
+    { href: "/tags", label: "tags" },
 ]
 
 export const SOCIAL_LINKS: Link[] = [
-  { href: 'https://github.com/choco-lily', label: 'GitHub' },
-  { href: 'https://x.com/Choco_Lily_', label: 'Twitter' },
-  { href: 'x@chocolily.dev', label: 'Email' },
-  { href: '/rss.xml', label: 'RSS' },
+    { href: "https://github.com/choco-lily", label: "GitHub" },
+    { href: "https://x.com/Choco_Lily_", label: "Twitter" },
+    { href: "x@chocolily.dev", label: "Email" },
+    { href: "/rss.xml", label: "RSS" },
 ]
--- src/content/authors/chocolily.md
+++ src/content/authors/chocolily.md
@@ -1,10 +1,10 @@
 ---
-name: 'chocolily'
+name: "chocolily"
 # pronouns: 'chocolily'
-avatar: 'https://chocolily.dev/sakura.jpg'
+avatar: "https://cdn.chocolily.dev/sakura.jpg"
 # bio: 'd(-_-)b'
-website: 'https://chocolily.dev'
-twitter: 'https://x.com/Choco_Lily__'
-github: 'https://github.com/choco-lily'
-mail: 'x@chocolily.dev'
+website: "https://chocolily.dev"
+twitter: "https://x.com/Choco_Lily__"
+github: "https://github.com/choco-lily"
+mail: "x@chocolily.dev"
 ---
